9d3c378adcd850e85bf37411559a0f1c9911b7fd
Jucactru <juancarlos@jucactru.com>
2018-01-17 23:57:08 -0500

    03 - se crea la base

---
+++ 03-stringIdioma/assets/js/platzom.js
@@ -0,0 +1,16 @@
+// Si la palabra original es un palíndromo,
+// ninguna regla anterior cuenta y se devuelve
+// la misma palabra intercalando mayúsculas y minúsculas
+
+//
+// Si la palabra termina en "ar", se le quitan esos dos caracteres
+//
+
+//
+// Si la palabra inicia con Z, se le añade "pe" al final
+//
+
+//
+//  Si la palabra traducida tiene 10 o más letras,
+// se debe partir a la mitad y unir con un guión del medio
+//
