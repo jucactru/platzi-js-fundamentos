b3176123ee68acac8c964cc2d75c8b3214415484
Jucactru <juancarlos@jucactru.com>
2018-01-18 23:39:11 -0500

    03 - se crea la regla final y funciones de reversa y MayMin

--- 03-stringIdioma/assets/js/platzom.js
+++ 03-stringIdioma/assets/js/platzom.js
@@ -1,6 +1,34 @@
-// Si la palabra original es un palíndromo,
-// ninguna regla anterior cuenta y se devuelve
-// la misma palabra intercalando mayúsculas y minúsculas
+/**
+ * [reverse Arrow Function que devuelve una cadena de texto en recersa]
+ * @param  {string}     str     [Cadena de texto a procesar]
+ * @return {string}             [Cadena procesada al reves]
+ */
+const reverseStr = (str) => str.split('').reverse().join('')
+
+/**
+ * [minMay Arrow Function que intercambia las letras entre mayúsculas y minúsculas]
+ * @param  {string}     str     [Cadena de texto a trabajar]
+ * @return {string}             [Cadena de texto ya procesada con mayúsculas y minúsculas]
+ */
+const minMayString = (str) => {
+  //creo la variable largo de cadena
+  var largoCadena = str.length
+  //reinicio la cadena traducida
+  let strTraduccion = ''
+  //creo la variable para mayúsuclas
+  let capitalize = true
+  //itero la data de str
+  for (let i = 0; i < largoCadena; i++) {
+    //tomo la letra (caracter) dentro de la cadena
+    var char = str.charAt(i)
+    //agrego el caracter a la cadena condicionando si se debe hacer en mayúscula o minúscula
+    strTraduccion += capitalize ? char.toUpperCase() : char.toLowerCase()
+    //cambio el valor de capitalize
+    capitalize = !capitalize
+  }
+  //devuelvo el valor traducido
+  return strTraduccion
+}
 
 
 /**
@@ -11,6 +39,16 @@
 function platzom(str) {
   //creo la variable traducción
   let strTraduccion = str;
+
+  //  ---------- REGLA ---------- //
+  // Si la palabra original es un palíndromo,
+  // ninguna regla anterior cuenta y se devuelve
+  // la misma palabra intercalando mayúsculas y minúsculas
+  //  --------------------------- //
+  if (str == reverseStr(str)) {
+    //convierto la cadena
+    return minMayString(str)
+  }
 
   //  ---------- REGLA ---------- //
   // Si la palabra termina en "ar",
@@ -52,3 +90,4 @@ console.log(platzom("Programar")) // Program
 console.log(platzom("Zorro")) // Zorrope
 console.log(platzom("Zarpar")) // Zarppe
 console.log(platzom("abecedario")) // abece-dario
+console.log(platzom("sometemos")) // SoMeTeMoS
