6cd9c012bedd82466569c9ef42cc90dff5f94c35
Jucactru <juancarlos@jucactru.com>
2018-01-20 13:12:59 -0500

    05 - Se crea el proceso para iniciar los peleadores

--- 05-gokuvssuperman/assets/js/pelea.js
+++ 05-gokuvssuperman/assets/js/pelea.js
@@ -1,1 +1,39 @@
-alert('inicia la pelea');
+/**
+ * [dataPeleadores data inicial de peleadores]
+ * @type {Array}
+ */
+const dataPeleadores = [{
+    nombre: 'Goku',
+    vida: 100
+  },
+  {
+    nombre: 'Superman',
+    vida: 100
+  }
+];
+
+/**
+ * [dataRealPeleadores Variable que contendrá a los peleadores para luchar]
+ * @type {Array}
+ */
+let dataRealPeleadores = [];
+
+/**
+ * [iniciarPeleadores Función que inicia a los peleadores en el sistema]
+ * @return {none}  [no hay retorno]
+ */
+const iniciarPeleadores = () => {
+  //valido si hay dataPeleadores
+  if (dataPeleadores != null) {
+    //itero la dataPeleadores
+    for (itemPeleador of dataPeleadores) {
+      //creo al peleadores
+      let objPeleador = new Peleador(itemPeleador);
+      //agrego el peleador en la data dataRealPeleadores
+      dataRealPeleadores.push(objPeleador);
+    }
+  }
+}
+
+//inicio los dataPeleadores
+iniciarPeleadores();
