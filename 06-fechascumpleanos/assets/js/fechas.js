017af560de0c5ce19f018427cb01511bf269aa19
Jucactru <juancarlos@jucactru.com>
2018-01-21 16:51:59 -0500

    06 - se crean las variables de dias, meses y fechas actual y cumpleaños

---
+++ 06-fechascumpleanos/assets/js/fechas.js
@@ -0,0 +1,19 @@
+/** -------------------------------------------------- **/
+/** -----------------CONSTANTES----------------------- **/
+/** -------------------------------------------------- **/
+
+/**
+ * [diasSemana Variable que almacena los días de la semana]
+ * @type {Array}
+ */
+const dataDias = ['Domingo', 'Lúnes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
+
+/**
+ * [dataMeses Variable que almacena los meses del año]
+ * @type {Array}
+ */
+const dataMeses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+const fechaActual = new Date();
+
+var fechaNaciemiento;
