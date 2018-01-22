c3f6351e9c4c991e66ac6da9b3f8cafa5810cbd5
Jucactru <juancarlos@jucactru.com>
2018-01-22 09:26:06 -0500

    06 - se finaliza el ejercicio

--- 06-fechascumpleanos/assets/js/fechas.js
+++ 06-fechascumpleanos/assets/js/fechas.js
@@ -43,6 +43,11 @@ const crearPersona = (pKey) => {
   return new Persona(dataPersonas[pKey]);
 }
 
+/**
+ * [datosNacimiento función que muestra los datos de fecha desde el nacimiento]
+ * @param  {Persona} pPersona [objeto persona]
+ * @return {none}             [no hay retorno]
+ */
 const datosNacimiento = (pPersona) => {
   //obtengo la fechaActual
   fechaActual = new Date();
@@ -56,14 +61,30 @@ const datosNacimiento = (pPersona) => {
   let fechaHoras = fechaMinutos / 60;
   console.log(`${fechaHoras.toFixed(2)} Horas`);
   let fechaDias = fechaHoras / 24;
+  console.log(`${fechaDias.toFixed(2)} Días`);
   let fechaAnios = fechaDias / 365;
   let fechaMeses = fechaAnios * 12;
-  console.log(`${fechaDias.toFixed(2)} Días`);
   console.log(`${fechaMeses.toFixed(2)} Meses`);
   console.log(`${fechaAnios.toFixed(2)} Años`);
-
 }
 
+
+const proximoCumpleanos = (pPersona) => {
+  //obtengo la fecha actual
+  fechaActual = new Date();
+  //valido si el mes ya pasó
+  if (fechaActual.getMonth() > pPersona.NACIMIENTO.getMonth()) {
+    //creo la fecha del próximo cumpleaños
+    let proximo = new Date((fechaActual.getFullYear() + 1), pPersona.NACIMIENTO.getMonth(), pPersona.NACIMIENTO.getDate());
+    console.log(`El día que cumple años es: ${dataDias[proximo.getDay()]} ${proximo.getDate()} de ${dataMeses[proximo.getMonth()]} del ${proximo.getFullYear()}`);
+
+  } else {
+    //creo la fecha del próximo cumpleaños
+    let proximo = new Date(fechaActual.getFullYear(), pPersona.NACIMIENTO.getMonth(), pPersona.NACIMIENTO.getDate());
+    console.log(`El día que cumple años es: ${dataDias[proximo.getDay()]} ${proximo.getDate()} de ${dataMeses[proximo.getMonth()]} del ${proximo.getFullYear()}`);
+  }
+}
 //creo a juan
 juan = crearPersona(0);
 datosNacimiento(juan);
+proximoCumpleanos(juan);
