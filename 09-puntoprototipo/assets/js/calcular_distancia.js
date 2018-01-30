4169275aa691d574582c1436dad19e28ba77885e
Jucactru <juancarlos@jucactru.com>
2018-01-29 21:18:55 -0500

    09 - se inicia el ejercicio con el código del 8 como base

---
+++ 09-puntoprototipo/assets/js/calcular_distancia.js
@@ -0,0 +1,48 @@
+/**
+ * [p1 posición 1]
+ * @type {Object}
+ */
+const p1 = {
+  x: 0,
+  y: 4,
+  moverEnX(x) {
+    this.x += x;
+  },
+  moverEnY(y) {
+    this.y += y;
+  }
+
+};
+
+/**
+ * [p2 posición 2]
+ * @type {Object}
+ */
+const p2 = {
+  x: 3,
+  y: 0,
+  moverEnX(x) {
+    this.x += x;
+  },
+  moverEnY(y) {
+    this.y += y;
+  }
+}
+
+/**
+ * [valorX función que calcula la distancia entre dos puntos cardinales]
+ * @param  {Object}   pPosicion1    [objeto con posiciones cardinales en X y Y]
+ * @param  {Object}   pPosicion2    [objeto con posiciones cardinales en X y Y]
+ * @return {Number}                 [Entero de la distancia]
+ */
+const calcularDistancia = (pPosicion1, pPosicion2) => {
+  //obtengp el valor x
+  let valorX = pPosicion1.x - pPosicion2.x
+  //obtengo el valor y
+  let valorY = pPosicion1.y - pPosicion2.y;
+  //retorno el calculo
+  return Math.sqrt(Math.pow(valorX, 2) + Math.pow(valorY, 2)).toFixed(2);
+}
+
+console.log(calcularDistancia(p1, p2));
+console.log(calcularDistancia(p2, p1));
