dbea1d6eabfb83be71520f432940f3355786be2f
Jucactru <juancarlos@jucactru.com>
2018-01-30 21:46:10 -0500

    11 - se crea la clase punto

---
+++ 11-puntoclass/assets/js/class/class_punto.js
@@ -0,0 +1,16 @@
+/**
+ * [Punto Objeto de tipo Punto]
+ * @author Juan Carlos Castañeda Trujillo <juancarlos@jucactru.com>
+ */
+class Punto {
+  /**
+   * [constructor]
+   * @param {Number} pX [valor de la posición x]
+   * @param {[type]} pY [valor de la posicion y]
+   */
+  constructor(pX, pY) {
+    this.x = pX;
+    this.y = pY;
+  }
+
+}
