c6b6f72db60834ee6ffe5715d70e8f54d86fd4d6
Jucactru <juancarlos@jucactru.com>
2018-02-11 23:47:40 -0500

    14 - se realiza el ejercicio

--- 14-callapply/assets/js/custom.js
+++ 14-callapply/assets/js/custom.js
@@ -1,6 +1,35 @@
+/**
+ * [juan objeto persona]
+ * @type {JSON}
+ */
 const juan = {
   nombre: 'Juan Carlos',
   apellido: 'Castañeda Trujillo'
 }
 
-console.log(juan)
+/**
+ * [saludar Función que realiza el saludo a una persona]
+ * @param  {Number}   pCantidad   [Cantidad de veces que se saludará]
+ * @param  {Boolean}  pUppercase  [Opción para saludar en altas o bajas]
+ * @return {None}                 [no hay retorno]
+ */
+const saludar = (pCantidad, pUppercase) => {
+  //creo la cadena de texto
+  let str = `Hola ${this.nombre} ${this.apellido}`
+  //cambio la cadena dependeidendo de parametro pUppercase
+  str = pUppercase ? str.toUpperCase() : str
+  //itero la cantidad de veces
+  for (let i = 0; i < pCantidad; i++) {
+    console.log(str);
+  }
+}
+
+console.log('---')
+//llamo la función con Call
+saludar.call(juan, 3, false)
+saludar.call(juan, 1, true)
+console.log('---')
+//llamo la funció con Apply
+saludar.apply(juan, [3, false]);
+saludar.apply(juan, [2, true]);
+console.log('---')
